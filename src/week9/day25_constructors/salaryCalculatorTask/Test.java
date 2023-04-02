6086df42427e1ced6769be1cf6f7b24eed9d252e
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2023-04-02 16:28:55 -0500

    week 9 code

---
+++ src/week9/day25_constructors/salaryCalculatorTask/Test.java
@@ -0,0 +1,11 @@
+package week9.day25_constructors.salaryCalculatorTask;
+
+public class Test {
+
+    public static void main(String[] args) {
+        SalaryCalculator obj = new SalaryCalculator(65,40,5.4, 25.5);
+
+        System.out.println(obj);
+    }
+
+}
